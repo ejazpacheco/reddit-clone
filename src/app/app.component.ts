714e4989d9b62e163fafe44507a3df4184d968e4
Ejaz Pacheco <ejaz.89@gmail.com>
2021-03-25 22:49:19 -0400

    Functionality complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article} from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('CNN', 'http://cnn.com', 0),
+      new Article('NBC', 'http://nbc.com', 4),
+      new Article('CP24', 'http://cp24.com', 6)
+    ]
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value,link.value));
+    return false;
+  }
+
+  sortedArticles() {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
+  }
+
 }
